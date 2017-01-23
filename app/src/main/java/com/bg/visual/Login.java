bd4cd9bb575cfa97a82290e075d18723d5485dc0
begemot695 <begemot695@yandex.ru>
2017-01-23 17:03:29 +0200

    OpenSave

--- app/src/main/java/com/bg/visual/Login.java
+++ app/src/main/java/com/bg/visual/Login.java
@@ -1,16 +1,23 @@
 package com.bg.visual;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
 
 public class Login extends AppCompatActivity {
 
-    @Override
+    SharedPreferences ShPerf;
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
@@ -21,16 +28,31 @@ public class Login extends AppCompatActivity {
         EditText userPassword = (EditText) findViewById(R.id.userPassword);
         String nameText = userName.getText().toString();
         String passwordtext = userPassword.getText().toString();
+        
+        ShPerf = getSharedPreferences("test",MODE_PRIVATE);
+        String SavedName = ShPerf.getString("Name","");
+        String SavedPwd =ShPerf.getString("Pwd","");
+
+       /* byte[] UName = Base64.decode(SavedPwd,Base64.DEFAULT);
+        String decodedPwd = " ";
+        try {
+            decodedPwd = new String(UName, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }*/
 
         if (nameText.equals("")||passwordtext.equals("")) {
             Toast toast = Toast.makeText(getApplicationContext(),
                     "Не заполнены поля", Toast.LENGTH_SHORT);
             toast.show();
-        }else if (nameText.equals("Ivan") && passwordtext.equals("123")){
+        }else if (nameText.equals(SavedName) && passwordtext.equals(SavedPwd)){
             Intent intent = new Intent(this, Welcom.class);
             intent.putExtra("Name", nameText);
-            intent.putExtra("Password", passwordtext);
             startActivity(intent);
+        }else{
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Не верно Имя или Пароль", Toast.LENGTH_SHORT);
+            toast.show();
         }
     }
 }
--- app/src/main/java/com/bg/visual/Registration.java
+++ app/src/main/java/com/bg/visual/Registration.java
@@ -1,19 +1,24 @@
 package com.bg.visual;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class Registration extends AppCompatActivity {
 
+    SharedPreferences shPref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
     }
+
     public void userReg(View view) {
         EditText userName = (EditText) findViewById(R.id.userName);
         EditText userMail = (EditText) findViewById(R.id.userEmail);
@@ -25,17 +30,31 @@ public class Registration extends AppCompatActivity {
         String PasswordText = userPassword.getText().toString();
         String ConfirmPasswordText = confirmPassword.getText().toString();
 
-        if (!(PasswordText.equals(ConfirmPasswordText))) {
-            Toast toast = Toast.makeText(getApplicationContext(),
-                    "Пароли не совпадают", Toast.LENGTH_SHORT);
+        if(NameText.equals("")&&MailText.equals("")&&PasswordText.equals("")&&ConfirmPasswordText.equals("")) {
+            Toast toast = Toast.makeText(this,"Не заполнены поля ", Toast.LENGTH_SHORT);
             toast.show();
+            }else if (!(PasswordText.equals(ConfirmPasswordText))) {
+                Toast toast = Toast.makeText(this,"Пароли не совпадают", Toast.LENGTH_SHORT);
+                toast.show();
+                } else {
 
-        } else {
-            Intent intent = new Intent(this,Welcom.class);
-            intent.putExtra("Name",NameText);
-            intent.putExtra("Mail",MailText);
-            startActivity(intent);
+                /*byte[] data = null;
+                data = PasswordText.getBytes();
+                String PwdText = Base64.encodeToString(data,Base64.DEFAULT);*/
 
-        }
+                Save(NameText,MailText,PasswordText);
+                Intent intent = new Intent(this,Welcom.class);
+                intent.putExtra("Name",NameText);
+                intent.putExtra("Mail",MailText);
+                startActivity(intent);
+                }
+    }
+    void Save(String Name,String Mail,String Password){
+        shPref = getSharedPreferences("test",MODE_PRIVATE);
+        SharedPreferences.Editor editor = shPref.edit();
+        editor.putString("Name", Name);
+        editor.putString("Mail", Mail);
+        editor.putString("Pwd", Password);
+        editor.apply();
     }
 }
