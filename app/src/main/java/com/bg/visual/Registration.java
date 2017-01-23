e1cfb576718aff013fbb5be40acaeb3d00dde93a
begemot695 <begemot695@yandex.ru>
2017-01-23 18:55:07 +0200

    OpenSave

--- app/src/main/java/com/bg/visual/Registration.java
+++ app/src/main/java/com/bg/visual/Registration.java
@@ -29,26 +29,29 @@ public class Registration extends AppCompatActivity {
         String MailText = userMail.getText().toString();
         String PasswordText = userPassword.getText().toString();
         String ConfirmPasswordText = confirmPassword.getText().toString();
-
+            //проверка на пустые поля
         if(NameText.equals("")&&MailText.equals("")&&PasswordText.equals("")&&ConfirmPasswordText.equals("")) {
             Toast toast = Toast.makeText(this,"Не заполнены поля ", Toast.LENGTH_SHORT);
             toast.show();
+            //проверка на совпадение паролей
             }else if (!(PasswordText.equals(ConfirmPasswordText))) {
                 Toast toast = Toast.makeText(this,"Пароли не совпадают", Toast.LENGTH_SHORT);
                 toast.show();
                 } else {
-
-                /*byte[] data = null;
+                //шифрование пароля
+                byte[] data = null;
                 data = PasswordText.getBytes();
-                String PwdText = Base64.encodeToString(data,Base64.DEFAULT);*/
-
-                Save(NameText,MailText,PasswordText);
+                String PwdText = Base64.encodeToString(data,Base64.DEFAULT);
+                // запись данных для дальнейшей проверки
+                Save(NameText,MailText,PwdText);
+                //отправка данных в другой активити
                 Intent intent = new Intent(this,Welcom.class);
                 intent.putExtra("Name",NameText);
                 intent.putExtra("Mail",MailText);
                 startActivity(intent);
                 }
     }
+        //сохранение данных
     void Save(String Name,String Mail,String Password){
         shPref = getSharedPreferences("test",MODE_PRIVATE);
         SharedPreferences.Editor editor = shPref.edit();
